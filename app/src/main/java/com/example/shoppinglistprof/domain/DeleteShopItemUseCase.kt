6e2e0dae27b942edb2b3dffc30ff8137626789c4
legom5 <19olegka97@mail.ru>
2023-10-06 16:14:45 +0300

    Domain 1

---
+++ app/src/main/java/com/example/shoppinglistprof/domain/DeleteShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglistprof.domain
+
+class DeleteShopItemUseCase {
+
+    fun deleteShopItem(shopItem: ShopItem){
+        TODO()
+    }
+}
---
+++ app/src/main/java/com/example/shoppinglistprof/domain/GetShopListUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.shoppinglistprof.domain
+
+class GetShopListUseCase {
+
+    fun getShopList(): List<ShopItem> {
+        TODO()
+    }
+}
