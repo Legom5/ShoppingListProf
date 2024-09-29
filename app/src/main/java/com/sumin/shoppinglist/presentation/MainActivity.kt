dc9d304e3fb4b58decde7c5a5c17e9dd22861a45
Legom5 <19olegka97@mail.ru>
2024-09-29 13:47:30 +0300

    finish presentation

--- app/src/main/java/com/sumin/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/sumin/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.sumin.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.sumin.shoppinglist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this){
+            if (count == 0){
+                count++
+                val item = it[0]
+                viewModel.deleteShopItem(item)
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/sumin/shoppinglist/presentation/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.sumin.shoppinglist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.sumin.shoppinglist.data.ShopListRepositoryImpl
+import com.sumin.shoppinglist.domain.DeleteShopItemUseCase
+import com.sumin.shoppinglist.domain.EditShopItemUseCase
+import com.sumin.shoppinglist.domain.GetShopListUseCase
+import com.sumin.shoppinglist.domain.ShopItem
+import com.sumin.shoppinglist.domain.ShopListRepository
+
+class MainViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = getShopListUseCase.getShopList()
+
+    fun changeEnabledState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        editShopItemUseCase.editShopItem(newItem)
+
+
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+
+    }
+
+
+}
